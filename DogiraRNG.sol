f677682d2eee75532423376c9d288bd66cdd7884
andremusk <78470314+andremusk@users.noreply.github.com>
2021-02-06 15:22:43 -0800

    Kovan RNG

---
+++ DogiraRNG.sol
@@ -0,0 +1,390 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+/*
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with GSN meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+interface LinkTokenInterface {
+  function allowance(address owner, address spender) external view returns (uint256 remaining);
+  function approve(address spender, uint256 value) external returns (bool success);
+  function balanceOf(address owner) external view returns (uint256 balance);
+  function decimals() external view returns (uint8 decimalPlaces);
+  function decreaseApproval(address spender, uint256 addedValue) external returns (bool success);
+  function increaseApproval(address spender, uint256 subtractedValue) external;
+  function name() external view returns (string memory tokenName);
+  function symbol() external view returns (string memory tokenSymbol);
+  function totalSupply() external view returns (uint256 totalTokensIssued);
+  function transfer(address to, uint256 value) external returns (bool success);
+  function transferAndCall(address to, uint256 value, bytes calldata data) external returns (bool success);
+  function transferFrom(address from, address to, uint256 value) external returns (bool success);
+}
+
+
+/*
+- Provides ownerOnly() modifier
+- Allows for ownership transfer but requires the newowner to claim (accept) ownership
+- Safer because no accidental transfers or renouncing
+*/
+contract Owned is Context {
+    address private _owner;
+    address private _pendingOwner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier ownerOnly {
+        require(_owner == _msgSender(), "Owner only");
+        _;
+    }
+    modifier pendingOnly {
+        require (_pendingOwner == msg.sender, "cannot claim");
+        _;
+    }
+
+    function pendingOwner() public view returns (address) {
+        return _pendingOwner;
+    }
+
+    function renounceOwnership() public virtual ownerOnly {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    function transferOwnership(address newOwner) public ownerOnly {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _pendingOwner = newOwner;
+    }
+
+    function cancelTransfer() public ownerOnly {
+        require(_pendingOwner != address(0), "no pending owner");
+        _pendingOwner = address(0);
+    }
+
+    function claimOwnership() public pendingOnly {
+        _pendingOwner = address(0);
+        emit OwnershipTransferred(_owner, _msgSender());
+        _owner = _msgSender();
+    }
+}
+
+
+contract VRFRequestIDBase {
+
+  /**
+   * @notice returns the seed which is actually input to the VRF coordinator
+   *
+   * @dev To prevent repetition of VRF output due to repetition of the
+   * @dev user-supplied seed, that seed is combined in a hash with the
+   * @dev user-specific nonce, and the address of the consuming contract. The
+   * @dev risk of repetition is mostly mitigated by inclusion of a blockhash in
+   * @dev the final seed, but the nonce does protect against repetition in
+   * @dev requests which are included in a single block.
+   *
+   * @param _userSeed VRF seed input provided by user
+   * @param _requester Address of the requesting contract
+   * @param _nonce User-specific nonce at the time of the request
+   */
+  function makeVRFInputSeed(bytes32 _keyHash, uint256 _userSeed,
+    address _requester, uint256 _nonce)
+    internal pure returns (uint256)
+  {
+    return  uint256(keccak256(abi.encode(_keyHash, _userSeed, _requester, _nonce)));
+  }
+
+  /**
+   * @notice Returns the id for this request
+   * @param _keyHash The serviceAgreement ID to be used for this request
+   * @param _vRFInputSeed The seed to be passed directly to the VRF
+   * @return The id for this request
+   *
+   * @dev Note that _vRFInputSeed is not the seed passed by the consuming
+   * @dev contract, but the one generated by makeVRFInputSeed
+   */
+  function makeRequestId(
+    bytes32 _keyHash, uint256 _vRFInputSeed) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked(_keyHash, _vRFInputSeed));
+  }
+}
+
+library SafeMathChainlink {
+  /**
+    * @dev Returns the addition of two unsigned integers, reverting on
+    * overflow.
+    *
+    * Counterpart to Solidity's `+` operator.
+    *
+    * Requirements:
+    * - Addition cannot overflow.
+    */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a, "SafeMath: addition overflow");
+
+    return c;
+  }
+
+  /**
+    * @dev Returns the subtraction of two unsigned integers, reverting on
+    * overflow (when the result is negative).
+    *
+    * Counterpart to Solidity's `-` operator.
+    *
+    * Requirements:
+    * - Subtraction cannot overflow.
+    */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a, "SafeMath: subtraction overflow");
+    uint256 c = a - b;
+
+    return c;
+  }
+
+  /**
+    * @dev Returns the multiplication of two unsigned integers, reverting on
+    * overflow.
+    *
+    * Counterpart to Solidity's `*` operator.
+    *
+    * Requirements:
+    * - Multiplication cannot overflow.
+    */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b, "SafeMath: multiplication overflow");
+
+    return c;
+  }
+
+  /**
+    * @dev Returns the integer division of two unsigned integers. Reverts on
+    * division by zero. The result is rounded towards zero.
+    *
+    * Counterpart to Solidity's `/` operator. Note: this function uses a
+    * `revert` opcode (which leaves remaining gas untouched) while Solidity
+    * uses an invalid opcode to revert (consuming all remaining gas).
+    *
+    * Requirements:
+    * - The divisor cannot be zero.
+    */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Solidity only automatically asserts when dividing by 0
+    require(b > 0, "SafeMath: division by zero");
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+    return c;
+  }
+
+  /**
+    * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+    * Reverts when dividing by zero.
+    *
+    * Counterpart to Solidity's `%` operator. This function uses a `revert`
+    * opcode (which leaves remaining gas untouched) while Solidity uses an
+    * invalid opcode to revert (consuming all remaining gas).
+    *
+    * Requirements:
+    * - The divisor cannot be zero.
+    */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0, "SafeMath: modulo by zero");
+    return a % b;
+  }
+}
+
+abstract contract VRFConsumerBase is VRFRequestIDBase {
+
+  using SafeMathChainlink for uint256;
+
+  /**
+   * @notice fulfillRandomness handles the VRF response. Your contract must
+   * @notice implement it. See "SECURITY CONSIDERATIONS" above for important
+   * @notice principles to keep in mind when implementing your fulfillRandomness
+   * @notice method.
+   *
+   * @dev VRFConsumerBase expects its subcontracts to have a method with this
+   * @dev signature, and will call it once it has verified the proof
+   * @dev associated with the randomness. (It is triggered via a call to
+   * @dev rawFulfillRandomness, below.)
+   *
+   * @param requestId The Id initially returned by requestRandomness
+   * @param randomness the VRF output
+   */
+  function fulfillRandomness(bytes32 requestId, uint256 randomness)
+    internal virtual;
+
+  /**
+   * @notice requestRandomness initiates a request for VRF output given _seed
+   *
+   * @dev The fulfillRandomness method receives the output, once it's provided
+   * @dev by the Oracle, and verified by the vrfCoordinator.
+   *
+   * @dev The _keyHash must already be registered with the VRFCoordinator, and
+   * @dev the _fee must exceed the fee specified during registration of the
+   * @dev _keyHash.
+   *
+   * @dev The _seed parameter is vestigial, and is kept only for API
+   * @dev compatibility with older versions. It can't *hurt* to mix in some of
+   * @dev your own randomness, here, but it's not necessary because the VRF
+   * @dev oracle will mix the hash of the block containing your request into the
+   * @dev VRF seed it ultimately uses.
+   *
+   * @param _keyHash ID of public key against which randomness is generated
+   * @param _fee The amount of LINK to send with the request
+   * @param _seed seed mixed into the input of the VRF.
+   *
+   * @return requestId unique ID for this request
+   *
+   * @dev The returned requestId can be used to distinguish responses to
+   * @dev concurrent requests. It is passed as the first argument to
+   * @dev fulfillRandomness.
+   */
+  function requestRandomness(bytes32 _keyHash, uint256 _fee, uint256 _seed)
+    internal returns (bytes32 requestId)
+  {
+    LINK.transferAndCall(vrfCoordinator, _fee, abi.encode(_keyHash, _seed));
+    // This is the seed passed to VRFCoordinator. The oracle will mix this with
+    // the hash of the block containing this request to obtain the seed/input
+    // which is finally passed to the VRF cryptographic machinery.
+    uint256 vRFSeed  = makeVRFInputSeed(_keyHash, _seed, address(this), nonces[_keyHash]);
+    // nonces[_keyHash] must stay in sync with
+    // VRFCoordinator.nonces[_keyHash][this], which was incremented by the above
+    // successful LINK.transferAndCall (in VRFCoordinator.randomnessRequest).
+    // This provides protection against the user repeating their input seed,
+    // which would result in a predictable/duplicate output, if multiple such
+    // requests appeared in the same block.
+    nonces[_keyHash] = nonces[_keyHash].add(1);
+    return makeRequestId(_keyHash, vRFSeed);
+  }
+
+  LinkTokenInterface immutable internal LINK;
+  address immutable private vrfCoordinator;
+
+  // Nonces for each VRF key from which randomness has been requested.
+  //
+  // Must stay in sync with VRFCoordinator[_keyHash][this]
+  mapping(bytes32 /* keyHash */ => uint256 /* nonce */) private nonces;
+
+  /**
+   * @param _vrfCoordinator address of VRFCoordinator contract
+   * @param _link address of LINK token contract
+   *
+   * @dev https://docs.chain.link/docs/link-token-contracts
+   */
+  constructor(address _vrfCoordinator, address _link) public {
+    vrfCoordinator = _vrfCoordinator;
+    LINK = LinkTokenInterface(_link);
+  }
+
+  // rawFulfillRandomness is called by VRFCoordinator when it receives a valid VRF
+  // proof. rawFulfillRandomness then calls fulfillRandomness, after validating
+  // the origin of the call
+  function rawFulfillRandomness(bytes32 requestId, uint256 randomness) external {
+    require(msg.sender == vrfCoordinator, "Only VRFCoordinator can fulfill");
+    fulfillRandomness(requestId, randomness);
+  }
+}
+
+interface IDogira {
+    function setRandomSeed(uint256 amount) external;
+}
+
+contract RandomNumberGenerator is Owned, VRFConsumerBase {
+
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+    bool public isDowntimePermitted;
+    address private dogiraAddress;
+
+
+    constructor()
+        VRFConsumerBase(
+            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+            0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
+        ) public
+    {
+        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK
+    }
+
+    /**
+     * Requests randomness from a user-provided seed
+     */
+    function getRandomNumber(uint256 userProvidedSeed) external returns (bytes32 requestId) {
+        require(msg.sender == address(dogiraAddress), "only dogira can call");
+        // in the case we run out of link we don't want downtime
+        if(!isDowntimePermitted && LINK.balanceOf(address(this)) <= fee && randomResult != 0) {
+            _pseudoFillRandomness(block.timestamp.add(randomResult.div(block.timestamp.add(block.number))));
+            return 0;
+        } else {
+            require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract first");
+            return requestRandomness(keyHash, fee, userProvidedSeed);
+        }
+    }
+
+    /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness;
+        IDogira(dogiraAddress).setRandomSeed(randomResult);
+    }
+
+    function _pseudoFillRandomness(uint256 value) internal {
+        randomResult = value;
+        IDogira(dogiraAddress).setRandomSeed(value.add(block.timestamp));
+    }
+
+    function setDowntimePermitted(bool value) external ownerOnly {
+        isDowntimePermitted = value;
+    }
+
+    function setDogira(address dogira) external ownerOnly {
+        dogiraAddress = dogira;
+    }
+}
